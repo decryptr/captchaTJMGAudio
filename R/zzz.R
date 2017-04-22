47ab9016c5c54512488e2043fef91549f13677fe
Julio Trecenti <julio.trecenti@gmail.com>
2017-04-22 13:59:49 -0300

    global vars

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+globalVariables(c("abs_som", "acu", "arq_aud", "corte", "head", "lg", "letra",
+                  "n", "plot", "r", "res", "revacu", "som", "soma", "", "tempo",
+                  "trocou", "xf", "xi", "."))
