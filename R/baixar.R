9e64942d084bfe9e193665095edab7a4e57b672d
Julio Trecenti <julio.trecenti@gmail.com>
2017-04-23 18:57:21 -0300

    no need to load page

--- R/baixar.R
+++ R/baixar.R
@@ -8,10 +8,10 @@
 #' @export
 baixar_img_audio <- function(dir = "data-raw/captcha") {
   if (!file.exists(dir)) dir.create(dir, recursive = TRUE)
-  url_pesq <- 'http://www4.tjmg.jus.br/juridico/sf/proc_resultado.jsp?tipoPesquisa=1&comrCodigo=701&txtProcesso=02775087620168130701&listaProcessos=02775087620168130701&nomePessoa=Nome+da+Pessoa&tipoPessoa=X&naturezaProcesso=0&situacaoParte=X&codigoOAB=&tipoOAB=N&ufOAB=MG&tipoConsulta=1&natureza=0&ativoBaixado=X&numero=1&select=1'
+  # url_pesq <- 'http://www4.tjmg.jus.br/juridico/sf/proc_resultado.jsp?tipoPesquisa=1&comrCodigo=701&txtProcesso=02775087620168130701&listaProcessos=02775087620168130701&nomePessoa=Nome+da+Pessoa&tipoPessoa=X&naturezaProcesso=0&situacaoParte=X&codigoOAB=&tipoOAB=N&ufOAB=MG&tipoConsulta=1&natureza=0&ativoBaixado=X&numero=1&select=1'
   url_img <- 'http://www4.tjmg.jus.br/juridico/sf/captcha.svl'
   url_aud <- 'http://www4.tjmg.jus.br/juridico/sf/captchaAudio.svl'
-  r0 <- httr::GET(url_pesq)
+  # r0 <- httr::GET(url_pesq)
   data_hora <- stringr::str_replace_all(lubridate::now(), "[^0-9]", "")
   arq <- tempfile(pattern = data_hora, tmpdir = dir)
   wd_img <- httr::write_disk(paste0(arq, ".jpg"), overwrite = TRUE)
